c83cb36695c2b826e25bba2d2a2ce71a4e8cc15f
MCVdev <mateocarvajalvergara333@gmail.com>
2025-10-14 18:31:01 -0500

    spinner implemented in all banks

--- pago/bancolombia/index.php
+++ pago/bancolombia/index.php
@@ -35,6 +35,11 @@
         100% { transform: rotate(360deg); }
     }
 
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+
 
   </style>
 
--- pago/occidente/index.php
+++ pago/occidente/index.php
@@ -28,6 +28,11 @@ E html>
         animation: spin 1s linear infinite;
         margin: 0 auto 3rem;
     }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
 </style>
 
 </head>
